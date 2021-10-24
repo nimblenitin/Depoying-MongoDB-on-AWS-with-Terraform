357d199479118f114a45f00c9c2a7cfc18ab97c4
Nitin Naidu <naidunitin1@gmail.com>
2021-10-24 13:15:49 +0530

    Update main.tf

--- mongodb_cluster/main.tf
+++ mongodb_cluster/main.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "ubuntu_18_image" {
   most_recent = true
   owners      = ["099720109477"]
-
+ 
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
